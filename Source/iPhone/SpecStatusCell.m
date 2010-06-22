e3f450f0de11032ce4770063eadc7eb6204103ff
Adam Milligan & Mike Gehard <adam+gmike@pivotallabs.com>
2010-06-22 14:10:47 -0700

    Add chevron to spec status cells that have children.

--- Source/iPhone/SpecStatusCell.m
+++ Source/iPhone/SpecStatusCell.m
@@ -1,6 +1,10 @@
 #import "SpecStatusCell.h"
 #import "CDRExampleBase.h"
 
+@interface SpecStatusCell (Private)
+- (void)setUpDisplayForExample:(CDRExampleBase *)example;
+@end
+
 @implementation SpecStatusCell
 
 @synthesize example = example_;
@@ -24,6 +28,7 @@
             backgroundColor = [UIColor redColor];
             break;
     }
+    [self.contentView setBackgroundColor:backgroundColor];
     [self setBackgroundColor:backgroundColor];
     [super drawRect:rect];
 }
@@ -33,7 +38,7 @@
         [example_ release];
         example_ = [example retain];
 
-        self.textLabel.text = example.text;
+        [self setUpDisplayForExample:example];
         [example_ addObserver:self forKeyPath:@"state" options:0 context:NULL];
     }
 }
@@ -41,6 +46,14 @@
 #pragma mark KVO
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
     [self performSelectorOnMainThread:@selector(setNeedsDisplay) withObject:NULL waitUntilDone:NO];
+}
+
+#pragma mark Private interface
+- (void)setUpDisplayForExample:(CDRExampleBase *)example {
+    self.textLabel.text = example.text;
+    if ([example_ hasChildren]) {
+        self.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    }
 }
 
 @end
