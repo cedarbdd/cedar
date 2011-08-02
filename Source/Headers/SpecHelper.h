375eb3157a454508a32fba0b72f90e28b9c237eb
Adam Milligan <adam@pivotallabs.com>
2011-08-02 15:46:15 -0700

    Removed useless method deprecations on SpecHelper::beforeEach and SpecHelper::afterEach; replaced with warnings logged to the console.

--- Source/Headers/SpecHelper.h
+++ Source/Headers/SpecHelper.h
@@ -12,10 +12,4 @@
 
 + (SpecHelper *)specHelper;
 
-// Rather than defining global beforeEach/afterEach on the SpecHelper instance, simply declare a
-// +beforeEach and/or +afterEach on a separate spec-specific class.  This allows for more than one
-// beforeEach/afterEach without them overwriting one another.
-- (void)beforeEach /*__attribute__((deprecated))*/;
-- (void)afterEach /*__attribute__((deprecated))*/;
-
 @end
--- Source/SpecHelper.m
+++ Source/SpecHelper.m
@@ -35,19 +35,32 @@ static SpecHelper *specHelper__;
     [super dealloc];
 }
 
-- (void)beforeEach {
-}
-
-- (void)afterEach {
-}
-
 #pragma mark CDRExampleParent
 - (void)setUp {
     [self.sharedExampleContext removeAllObjects];
+
+    if ([self respondsToSelector:@selector(beforeEach)]) {
+        NSLog(@"********************************************************************************");
+        NSLog(@"Cedar no longer runs beforeEach blocks defined on the SpecHelper class.\n");
+        NSLog(@"Rather than defining a global beforeEach on the SpecHelper instance,");
+        NSLog(@"declare a +beforeEach class method on a separate spec-specific class.");
+        NSLog(@"This allows for more than one beforeEach without them overwriting one another.");
+        NSLog(@"********************************************************************************");
+    }
+
     [self.globalBeforeEachClasses makeObjectsPerformSelector:@selector(beforeEach)];
 }
 
 - (void)tearDown {
+    if ([self respondsToSelector:@selector(afterEach)]) {
+        NSLog(@"********************************************************************************");
+        NSLog(@"Cedar no longer runs afterEach blocks defined on the SpecHelper class.\n");
+        NSLog(@"Rather than defining a global afterEach on the SpecHelper instance,");
+        NSLog(@"declare an +afterEach class method on a separate spec-specific class.");
+        NSLog(@"This allows for more than one afterEach without them overwriting one another.");
+        NSLog(@"********************************************************************************");
+    }
+
     [self.globalAfterEachClasses makeObjectsPerformSelector:@selector(afterEach)];
 }
 
