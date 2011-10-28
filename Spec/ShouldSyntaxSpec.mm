8b3df714053d11f15983d7db9d6eec7b1c83de34
Dmitriy Kalinin <cppforlife@gmail.com>
2011-10-27 22:41:36 -0700

    should/should_not syntax

---
+++ Spec/ShouldSyntaxSpec.mm
@@ -0,0 +1,47 @@
+#define CEDAR_MATCHERS_ALLOW_SHOULD
+#if TARGET_OS_IPHONE
+// Normally you would include this file out of the framework.  However, we're
+// testing the framework here, so including the file from the framework will
+// conflict with the compiler attempting to include the file from the project.
+#import "SpecHelper.h"
+#import "OCMock.h"
+#else
+#import <Cedar/SpecHelper.h>
+#import <OCMock/OCMock.h>
+#endif
+
+extern "C" {
+#import "ExpectFailureWithMessage.h"
+}
+
+using namespace Cedar::Matchers;
+
+SPEC_BEGIN(ShouldSyntaxSpec)
+
+describe(@"Should syntax", ^{
+    describe(@"should", ^{
+        it(@"should work with positive case", ^{
+            3 should equal(3);
+        });
+
+        it(@"should work with negative case", ^{
+            expectFailureWithMessage(@"Expected <3> to equal <4>", ^{
+                3 should equal(4);
+            });
+        });
+    });
+
+    describe(@"should_not", ^{
+        it(@"should work with positive case", ^{
+            3 should_not equal(4);
+        });
+
+        it(@"should work with negative case", ^{
+            expectFailureWithMessage(@"Expected <3> to not equal <3>", ^{
+                3 should_not equal(3);
+            });
+        });
+    });
+});
+
+SPEC_END
