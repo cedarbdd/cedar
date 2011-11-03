bf9e9f8e8c00276c488990c3357838b9e6656b10
David Whetstone <david@humblehacker.com>
2011-11-03 10:44:17 -0700

    Use completion handler to dismiss ExampleDetailsVC

--- Source/Headers/iPhone/CDRExampleDetailsViewController.h
+++ Source/Headers/iPhone/CDRExampleDetailsViewController.h
@@ -1,13 +1,20 @@
 #import <UIKit/UIKit.h>
 
+typedef void (^CDRExampleDetailsViewControllerCompletionHandler)(void);
+
 @class CDRExampleBase;
 
 @interface CDRExampleDetailsViewController : UIViewController {
     CDRExampleBase *example_;
     UINavigationBar *navigationBar_;
     UILabel *fullTextLabel_, *messageLabel_;
+    CDRExampleDetailsViewControllerCompletionHandler completion_;
 }
 
 - (id)initWithExample:(CDRExampleBase *)example;
+
+@property (nonatomic, copy) CDRExampleDetailsViewControllerCompletionHandler completion;
+           
+
 
 @end
