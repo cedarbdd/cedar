514dee37f69861bc1dbe43639907ad5cb1420a13
Pivotal Labs <pivotal@ortega.sf.pivotallabs.com>
2012-01-11 10:24:52 -0800

    Added Xcode templates

---
+++ CodeSnippetsAndTemplates/Templates/Cedar Domain Spec.xctemplate/___FILEBASENAME___Spec.mm
@@ -0,0 +1,18 @@
+#import <Cedar/SpecHelper.h>
+#import <OCMock/OCMock.h>
+
+#import "___FILEBASENAME___.h"
+
+using namespace Cedar::Matchers;
+
+SPEC_BEGIN(___FILEBASENAMEASIDENTIFIER___Spec)
+
+describe(@"___FILEBASENAMEASIDENTIFIER___", ^{
+    __block ___FILEBASENAMEASIDENTIFIER___ *model;
+
+    beforeEach(^{
+
+    });
+});
+
+SPEC_END
---
+++ CodeSnippetsAndTemplates/Templates/Cedar UI Spec.xctemplate/___FILEBASENAME___Spec.mm
@@ -0,0 +1,18 @@
+#import "SpecHelper.h"
+#import "OCMock.h"
+
+#import "___FILEBASENAME___.h"
+
+using namespace Cedar::Matchers;
+
+SPEC_BEGIN(___FILEBASENAMEASIDENTIFIER___Spec)
+
+describe(@"___FILEBASENAMEASIDENTIFIER___", ^{
+    __block ___FILEBASENAMEASIDENTIFIER___ *controller;
+
+    beforeEach(^{
+
+    });
+});
+
+SPEC_END
