9a838b5cd695bb4fef62a0772f4aa6b2b9428905
Dmitriy Kalinin <cppforlife@gmail.com>
2012-10-18 20:25:10 -0700

    add test to make sure we dont expose ObjC runtime via public headers

---
+++ Spec/ObjCHeadersSpec.mm
@@ -0,0 +1,13 @@
+#if TARGET_OS_IPHONE
+// Normally you would include this file out of the framework.  However, we're
+// testing the framework here, so including the file from the framework will
+// conflict with the compiler attempting to include the file from the project.
+#import "SpecHelper.h"
+#else
+#import <Cedar/SpecHelper.h>
+#endif
+
+// https://github.com/pivotal/cedar/issues/53 sums things up nicely
+#if defined(_OBJC_RUNTIME_H) || defined(_OBJC_MESSAGE_H)
+#error Objective-C runtime headers should not be exposed via public headers
+#endif
