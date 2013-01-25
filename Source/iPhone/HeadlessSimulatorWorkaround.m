ad4fd5bc1f2e7b092e870babcaa9c341784955c0
Andrew Kitchen and Philip Kuryloski <pair+akitchen+pkuryloski@pivotallabs.com>
2013-01-25 11:00:47 -0800

    Wraps the fake simulator setup in an autorelase pool

--- Source/iPhone/HeadlessSimulatorWorkaround.m
+++ Source/iPhone/HeadlessSimulatorWorkaround.m
@@ -3,6 +3,7 @@
 
 void setUpFakeWorkspaceIfRequired() {
 #if TARGET_IPHONE_SIMULATOR
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
     NSString *systemVersion = [[UIDevice currentDevice] systemVersion];
     NSInteger majorVersion = [[[systemVersion componentsSeparatedByString:@"."] objectAtIndex:0] integerValue];
     if (majorVersion >= 6 && CFMessagePortCreateRemote(NULL, (CFStringRef)@"PurpleWorkspacePort") == NULL) {
@@ -10,5 +11,6 @@ void setUpFakeWorkspaceIfRequired() {
         CFMessagePortCreateLocal(NULL, (CFStringRef)@"PurpleWorkspacePort", NULL, NULL, NULL);
         class_replaceMethod([UIWindow class], @selector(_createContext), imp_implementationWithBlock(^{}), "v@:");
     }
+    [pool drain];
 #endif
 }
