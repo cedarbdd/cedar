9b91d79343167965895bbd7baa7759a422891c9f
Alex Basson and Sam Coward <pair+abasson+scoward@pivotallabs.com>
2013-04-25 10:06:32 -0400

    Implement -[CedarApplicationDelegate window]
    - It throws an exception informing the user that s?he is not actually asking
      h(er|is) own app's delegate for the window.
    - Stubbing is suggested to reveal intent in tests more clearly.

--- Source/iPhone/CedarApplicationDelegate.m
+++ Source/iPhone/CedarApplicationDelegate.m
@@ -61,4 +61,10 @@ void exitWithStatusFromUIApplication(int status) {
     return NO;
 }
 
+- (UIWindow *)window {
+    @throw [NSException exceptionWithName:NSInternalInconsistencyException
+                                   reason:@"This Cedar iOS spec suite is run with the CedarApplicationDelegate.  If your code needs the UIApplicationDelegate's window, you should stub this method to return an appropriate window."
+                                 userInfo:nil];
+}
+
 @end
---
+++ Spec/iPhone/CedarApplicationDelegateSpec.mm
@@ -0,0 +1,23 @@
+#import "SpecHelper.h"
+#import "CedarApplicationDelegate.h"
+
+using namespace Cedar::Matchers;
+using namespace Cedar::Doubles;
+
+SPEC_BEGIN(CedarApplicationDelegateSpec)
+
+describe(@"CedarApplicationDelegate", ^{
+    __block CedarApplicationDelegate *delegate;
+
+    beforeEach(^{
+        delegate = [[[CedarApplicationDelegate alloc] init] autorelease];
+    });
+
+    describe(@"-window", ^{
+        it(@"should raise an educational exception", ^{
+            ^{ [delegate window]; } should raise_exception.with_reason(@"This Cedar iOS spec suite is run with the CedarApplicationDelegate.  If your code needs the UIApplicationDelegate's window, you should stub this method to return an appropriate window.");
+        });
+    });
+});
+
+SPEC_END
