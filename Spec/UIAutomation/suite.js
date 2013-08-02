87f87ec24e6e083ed8d3b4a0aae618883d0d701c
Sam Coward <pair+scoward@pivotallabs.com>
2013-08-02 16:02:02 -0400

    Removed jasmine-iphone UIAutomation suite

--- Spec/UIAutomation/suite.js
+++
@@ -1,8 +0,0 @@
-#import "jasmine-iphone/jasmine-uiautomation.js"
-#import "jasmine-iphone/jasmine-uiautomation-reporter.js"
-
-// Import your JS spec files here.
-#import "uiautomation-spec.js"
-
-jasmine.getEnv().addReporter(new jasmine.UIAutomation.Reporter());
-jasmine.getEnv().execute();
