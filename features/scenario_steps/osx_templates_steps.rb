c032c4d574f35c31bc0b48fe3c4fe0649fba4a01
Sam Coward <idoru42@gmail.com>
2013-11-01 00:05:56 -0400

    Output build output when testing rakefile fails

--- features/scenario_steps/osx_templates_steps.rb
+++ features/scenario_steps/osx_templates_steps.rb
@@ -18,6 +18,10 @@ When(/^I add an OS X Spec suite target$/) do
 end
 
 Then(/^the `rake Specs` should work$/) do
-  `cd template-project; cd Specs; rake Specs 2> /dev/null`
+  output = `cd template-project; cd Specs; rake Specs 2> /dev/null`
+  if $? != 0
+    puts "!!! Spec target failed. Build output:"
+    puts output
+  end
   $?.exitstatus.should == 0
 end
