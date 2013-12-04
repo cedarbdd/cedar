5706640b65a7b27545d888c6c85d9094b64cca8c
Jesse Bounds and Tim Jarratt <pair+boundsj+tjarratt@pivotallabs.com>
2013-12-04 10:50:15 -0800

    Add failing 'spec suite' and 'spec bundle' tests for iOS rake templates
    - Update cucumber tests to cover these scenarios
    
    [#59629092]

--- features/ios_templates.feature
+++ features/ios_templates.feature
@@ -20,3 +20,18 @@ Feature:
     When I add an iOS Spec bundle target
     And I reference AppDelegate in the test
     Then the `rake Specs` should work
+
+  Scenario: Failing Spec suite tests
+    Given an Xcode iOS project
+
+    When I add an iOS Spec suite target
+    And I add a failing test
+    Then running the specs from the rake task should fail
+
+  Scenario: Failing spec bundle tests
+    Given an Xcode iOS project
+  
+    When I add an iOS Spec bundle target
+    And I add a failing test
+    Then running the specs from the rake task should fail
+
--- features/scenario_steps/ios_templates_steps.rb
+++ features/scenario_steps/ios_templates_steps.rb
@@ -21,6 +21,10 @@ When(/^I reference AppDelegate in the test$/) do
   `cp features/support/templates/ExampleSpec.mm template-project/Specs`
 end
 
+When(/^I add a failing test$/) do
+  `cp features/support/templates/FailingSpec.mm template-project/Specs/ExampleSpec.mm`
+end
+
 Then(/^the `rake Specs` should work$/) do
   Dir.chdir('template-project/Specs') do
     output = `rake Specs 2> /dev/null`
@@ -28,6 +32,13 @@ Then(/^the `rake Specs` should work$/) do
       puts "!!! Spec target failed. Build output:"
       puts output
     end
-    $?.exitstatus.should == 0
+    $?.exitstatus.should eq(0)
+  end
+end
+
+Then(/^running the specs from the rake task should fail$/) do
+  Dir.chdir('template-project/Specs') do
+    `rake Specs 2> /dev/null`
+    $?.exitstatus.should_not eq(0)
   end
 end
