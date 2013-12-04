4bceb0df5b7136a1d8035b3054c29a40277e00c7
Jesse Bounds and Tim Jarratt <pair+boundsj+tjarratt@pivotallabs.com>
2013-12-04 14:23:49 -0800

    Add failing 'spec suite' and 'spec bundle' tests for OSX rake templates
    - Update cucumber tests to cover these scenarios
    - Tests failed for bundles in xcode5, this is now fixed
    
    [#59629092]

--- features/osx_templates.feature
+++ features/osx_templates.feature
@@ -14,4 +14,17 @@ Feature:
     When I add an OS X Spec suite target
     Then the `rake Specs` should work
 
+  Scenario: Failing Spec suite tests
+    Given an Xcode OS X project
+
+    When I add an OS X Spec suite target
+    And I add a failing test
+    Then running the specs from the rake task should fail
+
+  Scenario: Failing Spec bundle tests
+    Given an Xcode OS X project
+
+    When I add an OS X Spec bundle target
+    And I add a failing test
+    Then running the specs from the rake task should fail
 
--- features/scenario_steps/osx_templates_steps.rb
+++ features/scenario_steps/osx_templates_steps.rb
@@ -17,6 +17,10 @@ When(/^I add an OS X Spec suite target$/) do
   File.exist?("template-project/Specs").should be_true
 end
 
+When(/^I add a failing test$/) do
+  `cp features/support/templates/FailingSpec.mm template-project/Specs/ExampleSpec.mm`
+end
+
 Then(/^the `rake Specs` should work$/) do
   Dir.chdir('template-project/Specs') do
     output = `rake Specs 2> /dev/null`
@@ -27,3 +31,11 @@ Then(/^the `rake Specs` should work$/) do
     $?.exitstatus.should == 0
   end
 end
+
+Then(/^running the specs from the rake task should fail$/) do
+  Dir.chdir('template-project/Specs') do
+    `rake Specs 2> /dev/null`
+    $?.exitstatus.should_not eq(0)
+  end
+end
+
