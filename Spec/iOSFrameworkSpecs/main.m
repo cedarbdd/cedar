fba42304887330c3671f3db3e9b397fa5f7ac2c2
Sam Coward <idoru42@gmail.com>
2013-12-05 11:48:43 -0500

    Revert "Added a target for testing Cedar-iOS.framework"
    This reverts commit d41fb129f42b1bf8f5ad5b507d6a764b4c9e8573.

--- Spec/iOSFrameworkSpecs/main.m
+++
@@ -1,7 +0,0 @@
-#import <Cedar-iOS/SpecHelper.h>
-
-int main(int argc, char *argv[]) {
-    @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, @"CedarApplicationDelegate");
-    }
-}
