d93084696df510f70b49f8655e03d98d8c94575f
Brian Croom <bcroom@pivotallabs.com>
2014-05-02 09:11:15 -0400

    Allow specifying shared example context in-line

--- Source/Headers/CDRSharedExampleGroupPool.h
+++ Source/Headers/CDRSharedExampleGroupPool.h
@@ -1,15 +1,19 @@
 #import <Foundation/Foundation.h>
 
+#define CDR_OVERLOADABLE __attribute__((overloadable))
+
 @protocol CDRSharedExampleGroupPool
 @end
 
 typedef void (^CDRSharedExampleGroupBlock)(NSDictionary *);
+typedef void (^CDRSharedExampleContextProviderBlock)(NSMutableDictionary *);
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 void sharedExamplesFor(NSString *, CDRSharedExampleGroupBlock);
-void itShouldBehaveLike(NSString *);
+CDR_OVERLOADABLE void itShouldBehaveLike(NSString *);
+CDR_OVERLOADABLE void itShouldBehaveLike(NSString *, CDRSharedExampleContextProviderBlock);
 #ifdef __cplusplus
 }
 #endif
