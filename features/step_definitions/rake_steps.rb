314f6819cae8f3bf1dc8b89b7acca70b660f2ab4
Jeff Hui and Tim Jarratt <tjarratt@pivotallabs.com>
2014-09-12 14:25:36 -0700

    Upgrade gems
    Also update cucumber tests to use new matcher syntax
    
    This was failing on CI for suspicious reasons.

--- features/step_definitions/rake_steps.rb
+++ features/step_definitions/rake_steps.rb
@@ -5,13 +5,13 @@ Then(/^the `rake Specs` should work$/) do
       puts "!!! Spec target failed. Build output:"
       puts output
     end
-    $?.exitstatus.should eq(0)
+    expect($?.exitstatus).to eq(0)
   end
 end
 
 Then(/^running the specs from the rake task should fail$/) do
   Dir.chdir('template-project/Specs') do
     `rake Specs 2> /dev/null`
-    $?.exitstatus.should_not eq(0)
+    expect($?.exitstatus).to_not eq(0)
   end
 end
--- features/step_definitions/xcode_steps.rb
+++ features/step_definitions/xcode_steps.rb
@@ -2,12 +2,12 @@ When(/^I add an (iOS|OS X) Spec (suite|bundle) target$/) do |os, target_type|
   xcode = File.absolute_path(File.join(`xcode-select -p`.strip, '../..'))
   `open -a #{xcode.inspect} template-project/template-project.xcodeproj`
   `osascript features/support/scripts/#{os.downcase.gsub(/\s+/, '')}_add_spec_#{target_type.downcase.gsub(/\s+/, '')}.applescript`
-  File.exist?("template-project/Specs").should be_true
+  expect(File.exist?("template-project/Specs")).to be_truthy
 end
 
 Given(/^an Xcode (iOS|OS X) project$/) do |os|
   `rm -rf template-project`
   `cp -pr features/support/#{os.downcase.gsub(/\s+/, '')}-project-template template-project`
-  $?.exitstatus.should == 0
-  File.exist?("template-project").should be_true
+  expect($?.exitstatus).to eq(0)
+  expect(File.exist?("template-project")).to be_truthy
 end
