6b058b4b54a638d8ede5ae17ee5fd6b89c9ec25e
Jeff Hui and Tim Jarratt <jhui@pivotallabs.com>
2014-09-19 15:38:22 -0700

    Kill simulator before each feature spec
    Otherwise you get into an awful state when switching between XCode
    versions.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,3 +1,4 @@
 puts "Installing latest templates, one moment..."
 `osascript -e 'tell application "Xcode"' -e 'quit' -e 'end tell'`
+`osascript -e 'tell application "iOS Simulator"' -e 'quit' -e 'end tell'`
 `rake install`
