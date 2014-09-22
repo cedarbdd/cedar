f06d9082fce4ef98c4204fb172ba7a5cb3b50cca
Jeff Hui and Tim Jarratt <tjarratt@pivotallabs.com>
2014-09-22 10:38:52 -0700

    OS X, iOS template should show up under correct groups
    When we added support for the templates on Xcode 6, somehow the OS X
    Spec Suite and the iOS Testing Bundle started showing up under both OS X
    and iOS groups for Cedar (which is bad).
    
    Setting the platform for this (which is available in xcode6) seems to
    fix it.

--- features/step_definitions/xcode_steps.rb
+++ features/step_definitions/xcode_steps.rb
@@ -1,6 +1,6 @@
+require 'set'
+
 When(/^I add an (iOS|OS X) (Spec Suite|Testing Bundle) target$/) do |os, target_type|
-  xcode = File.absolute_path(File.join(`xcode-select -p`.strip, '../..'))
-  `open -a #{xcode.inspect} template-project/template-project.xcodeproj`
   template_name = "#{os} Cedar #{target_type.downcase}"
   `osascript features/support/scripts/add_target.applescript #{os.downcase.inspect} #{template_name.inspect}`
   expect(File.exist?("template-project/Specs")).to be_truthy
@@ -12,4 +12,12 @@ Given(/^an Xcode (iOS|OS X) project$/) do |os|
   `cp -pr features/support/#{os.downcase.gsub(/\s+/, '')}-project-template template-project`
   expect($?.exitstatus).to eq(0)
   expect(File.exist?("template-project")).to be_truthy
+
+  xcode = File.absolute_path(File.join(`xcode-select -p`.strip, '../..'))
+  `open -a #{xcode.inspect} template-project/template-project.xcodeproj`
+end
+
+Then(/^I should only see the (iOS|OS X) Targets$/) do |os|
+  targets = `osascript features/support/scripts/list_targets.applescript #{os.downcase.inspect}`.chomp.split(',')
+  expect(targets.to_set).to eq(["#{os} Cedar Testing Bundle", "#{os} Cedar Spec Suite"].to_set)
 end
