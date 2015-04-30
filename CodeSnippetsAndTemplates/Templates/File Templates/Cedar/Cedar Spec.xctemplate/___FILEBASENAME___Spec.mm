c18e6945f6f9ad84c6c1e6e9f039cf61c57467a3
Tim Jarratt <tjarratt@gmail.com>
2015-04-30 15:00:46 -0700

    Update templates to work with Xcode 6.3
    Untested with earlier versions of xcode.
    
    Apparently in Xcode 6.3 the TemplateInfo.plist has a version of the macros that
    xcode provides (e.g.:: ___FILENAMEBASE___) that are different from the contents of
    the template. The template contents use the *actual* file name, rather than what the
    user provides.
    
    This fix just captures the class the user typed as a variable, and then uses it
    in the template. I believe this should work on earlier versions of Xcode, but
    I haven't had the liberty to check yet.
    
    Fixes #324

--- CodeSnippetsAndTemplates/Templates/File Templates/Cedar/Cedar Spec.xctemplate/___FILEBASENAME___Spec.mm
+++ CodeSnippetsAndTemplates/Templates/File Templates/Cedar/Cedar Spec.xctemplate/___FILEBASENAME___Spec.mm
@@ -1,13 +1,13 @@
 #import <Cedar/Cedar.h>
-#import "___FILEBASENAME___.h"
+#import "___VARIABLE_cedarClass:identifier___.h"
 
 using namespace Cedar::Matchers;
 using namespace Cedar::Doubles;
 
-SPEC_BEGIN(___FILEBASENAMEASIDENTIFIER___Spec)
+SPEC_BEGIN(___VARIABLE_cedarClass:identifier___Spec)
 
-describe(@"___FILEBASENAMEASIDENTIFIER___", ^{
-    __block ___FILEBASENAMEASIDENTIFIER___ *<#object under test#>;
+describe(@"___VARIABLE_cedarClass:identifier___", ^{
+    __block ___VARIABLE_cedarClass:identifier___ *<#object under test#>;
 
     beforeEach(^{
 
