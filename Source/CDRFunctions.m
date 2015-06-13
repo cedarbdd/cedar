37ed287f221f2e04d0e0ec89338d632d7920f2c9
Tim Jarratt <tjarratt@gmail.com>
2015-06-13 13:10:25 -0700

    Hack together support for XCTest under Xcode7
    Leaving the old hacks for Xcode 6 since the XCTestObservation protocol
    was previously deprecated, behaved differently then, and the observation
    center had not been introduced then.

--- Source/CDRFunctions.m
+++ Source/CDRFunctions.m
@@ -362,83 +362,16 @@ int CDRRunSpecs() {
     }
 }
 
-#pragma mark - Running Test Bundles
-#import "CDRXTestSuite.h"
-#import "CDRRuntimeUtilities.h"
-
-@interface CDRXCTestSupport : NSObject
-- (id)testSuiteWithName:(NSString *)name;
-- (id)defaultTestSuite;
-- (id)initWithName:(NSString *)aName;
-
-- (id)CDR_original_defaultTestSuite;
-
-- (void)addTest:(id)test;
-@end
-
-static id CDRCreateXCTestSuite() {
-    Class testSuiteClass = NSClassFromString(@"XCTestSuite") ?: NSClassFromString(@"SenTestSuite");
-    Class testSuiteSubclass = NSClassFromString(@"_CDRXTestSuite");
-
-    if (testSuiteSubclass == nil) {
-        size_t size = class_getInstanceSize([CDRXTestSuite class]) - class_getInstanceSize([NSObject class]);
-        testSuiteSubclass = objc_allocateClassPair(testSuiteClass, "_CDRXTestSuite", size);
-        CDRCopyClassInternalsFromClass([CDRXTestSuite class], testSuiteSubclass);
-        objc_registerClassPair(testSuiteSubclass);
-    }
-
-    id testSuite = [[[(id)testSuiteSubclass alloc] initWithName:@"Cedar"] autorelease];
-    CDRDefineSharedExampleGroups();
-    CDRDefineGlobalBeforeAndAfterEachBlocks();
-
-    unsigned int seed = CDRGetRandomSeed();
-
-    NSArray *specClasses = CDRSpecClassesToRun();
-    NSArray *permutedSpecClasses = CDRPermuteSpecClassesWithSeed(specClasses, seed);
-    NSArray *specs = CDRSpecsFromSpecClasses(permutedSpecClasses);
-    CDRMarkFocusedExamplesInSpecs(specs);
-    CDRMarkXcodeFocusedExamplesInSpecs(specs, [[NSProcessInfo processInfo] arguments]);
-
-    CDRReportDispatcher *dispatcher = [[[CDRReportDispatcher alloc] initWithReporters:CDRReportersToRun()] autorelease];
-
-    [testSuite setDispatcher:dispatcher];
-
-    NSArray *groups = CDRRootGroupsFromSpecs(specs);
-    [dispatcher runWillStartWithGroups:groups andRandomSeed:seed];
-
-    for (CDRSpec *spec in specs) {
-        [testSuite addTest:[spec testSuiteWithRandomSeed:seed dispatcher:dispatcher]];
-    }
-    return testSuite;
-}
-
-void CDRInjectIntoXCTestRunner() {
-    Class testSuiteClass = NSClassFromString(@"XCTestSuite") ?: NSClassFromString(@"SenTestSuite");
-
-    if (!testSuiteClass) {
-        [[NSException exceptionWithName:@"CedarNoTestFrameworkAvailable" reason:@"You must link against either XCTest or SenTestingKit frameworks." userInfo:nil] raise];
-    }
-
-    Class testSuiteMetaClass = object_getClass(testSuiteClass);
-    Method m = class_getClassMethod(testSuiteClass, @selector(defaultTestSuite));
-    class_addMethod(testSuiteMetaClass, @selector(CDR_original_defaultTestSuite), method_getImplementation(m), method_getTypeEncoding(m));
-    IMP newImp = imp_implementationWithBlock(^id(id self){
-        id defaultSuite = [self CDR_original_defaultTestSuite];
-        [defaultSuite addTest:CDRCreateXCTestSuite()];
-        return defaultSuite;
-    });
-    class_replaceMethod(testSuiteMetaClass, @selector(defaultTestSuite), newImp, method_getTypeEncoding(m));
-}
-
 NSString *CDRGetTestBundleExtension() {
     NSString *extension = nil;;
-
     NSArray *arguments = [[NSProcessInfo processInfo] arguments];
     NSSet *xctestFlags = [NSSet setWithArray:@[@"-XCTest", @"-XCTestScopeFile"]];
     if ([xctestFlags intersectsSet:[NSSet setWithArray:arguments]]) {
         extension = @".xctest";
     } else if ([arguments containsObject:@"-SenTest"]) {
         extension = @".octest";
+    } else if ((BOOL)NSClassFromString(@"XCTestCase")) {
+        extension = @".xctest";
     }
 
     return extension;
