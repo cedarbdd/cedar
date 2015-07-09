9eddb9e22c9ea73a2e1998cae66b2601f3d6cbb0
Juan David Dominguez and Tim Jarratt <tjarratt@pivotal.io>
2015-07-09 14:48:33 -0700

    Add a separate target for the Cedar Runner dynamic framework

---
+++ CedarRunner-iOS-Framework/CedarRunner.h
@@ -0,0 +1,21 @@
+#pragma mark - Cedar Core
+
+#import "CDRVersion.h"
+
+
+#pragma mark - Cedar Runner
+
+#import "CDRSpec.h"
+#import "CDRHooks.h"
+#import "CDRExample.h"
+#import "CDRFunctions.h"
+#import "CDRSpecHelper.h"
+#import "CDRSpecFailure.h"
+#import "CDRExampleBase.h"
+#import "CDRExampleGroup.h"
+#import "CDRExampleParent.h"
+#import "CDRSharedExampleGroupPool.h"
+
+#if TARGET_OS_IPHONE
+#import "CedarApplicationDelegate.h"
+#endif
