dd8160d80d2c45dd3f8d51f62334c508ed958297
Juan David Dominguez and Tim Jarratt <tjarratt@pivotal.io>
2015-07-09 10:50:07 -0700

    Cleanup, organize and document Cedar umbrella header
    This was feeling a bit like a dumping ground for all the various parts of
    Cedar. Organizing it makes it easier to search and understand what is being
    exposed to users.

--- Source/Headers/Cedar.h
+++ Source/Headers/Cedar.h
@@ -2,74 +2,73 @@
 #error Cedar may only be imported from Objective-C++ (.mm) files.
 #endif
 
+#pragma mark - Cedar Core
+
+#import "CDRVersion.h"
+
+
+#pragma mark - Cedar Runner
+
+#import "CDRSpec.h"
+#import "CDRHooks.h"
+#import "CDRExample.h"
 #import "CDRFunctions.h"
 #import "CDRSpecHelper.h"
-
-#if TARGET_OS_IPHONE
-#import "CedarApplicationDelegate.h"
-#import "Cedar-iOS.h"
-#endif
-
-#import "NSInvocation+Cedar.h"
-#import "Argument.h"
-#import "ValueArgument.h"
-#import "AnyInstanceArgument.h"
-#import "ReturnValue.h"
-#import "AnyArgument.h"
-#import "AnyInstanceOfClassArgument.h"
-#import "AnyInstanceConformingToProtocolArgument.h"
-#import "CDRProtocolFake.h"
-#import "CedarDoubles.h"
-#import "CDRSpy.h"
-#import "CDRClassFake.h"
-#import "HaveReceived.h"
-#import "CedarDouble.h"
-#import "RejectedMethod.h"
-#import "StubbedMethod.h"
-#import "CDRFake.h"
-#import "Base.h"
-#import "BeCloseTo.h"
-#import "BeFalsy.h"
-#import "BeGreaterThan.h"
-#import "BeGTE.h"
-#import "BeInstanceOf.h"
-#import "BeLessThan.h"
-#import "BeLTE.h"
-#import "BeNil.h"
-#import "BeSameInstanceAs.h"
-#import "BeTruthy.h"
-#import "ConformTo.h"
-#import "Equal.h"
-#import "Exist.h"
-#import "RaiseException.h"
-#import "RespondTo.h"
-#import "StringifiersBase.h"
-#import "StringifiersContainer.h"
-#import "ComparatorsBase.h"
-#import "ComparatorsContainer.h"
-#import "CompareEqual.h"
-#import "CompareGreaterThan.h"
-#import "ComparatorsContainerConvenience.h"
-#import "CompareCloseTo.h"
-#import "ActualValue.h"
-#import "CedarMatchers.h"
-#import "CedarComparators.h"
-#import "CedarStringifiers.h"
-#import "ShouldSyntax.h"
-#import "CDRVersion.h"
-#import "CDRExample.h"
+#import "CDRSpecFailure.h"
 #import "CDRExampleBase.h"
 #import "CDRExampleGroup.h"
 #import "CDRExampleParent.h"
-#import "CDRHooks.h"
 #import "CDRSharedExampleGroupPool.h"
-#import "CDRSpec.h"
-#import "CDRSpecFailure.h"
-#import "CDRTypeUtilities.h"
-#import "CDRSpyInfo.h"
+
+#if TARGET_OS_IPHONE
+#import "Cedar-iOS.h"
+#endif
+
+
+#pragma mark - Cedar Runner/Matchers
+
+#import "Equal.h"
+#import "BeCloseTo.h"
+#import "ActualValue.h"
+#import "ShouldSyntax.h"
+
+
+#pragma mark - Cedar Matchers
+
+#import "Base.h"
+#import "BeLTE.h"
+#import "BeNil.h"
+#import "BeGTE.h"
+#import "Exist.h"
 #import "BeEmpty.h"
 #import "Contain.h"
+#import "BeFalsy.h"
+#import "BeTruthy.h"
+#import "ConformTo.h"
+#import "RespondTo.h"
+#import "BeLessThan.h"
+#import "BeInstanceOf.h"
 #import "AnInstanceOf.h"
+#import "BeGreaterThan.h"
+#import "RaiseException.h"
+#import "BeSameInstanceAs.h"
+#import "StringifiersBase.h"
+#import "StringifiersContainer.h"
+
+#ifdef CEDAR_CUSTOM_STRINGIFIERS
+#import CEDAR_CUSTOM_STRINGIFIERS
+#endif
+
+
+#pragma mark - Cedar Matcher Comparators
+
+#import "CompareEqual.h"
+#import "CompareCloseTo.h"
+#import "ComparatorsBase.h"
+#import "CedarComparators.h"
+#import "CompareGreaterThan.h"
+#import "ComparatorsContainer.h"
+#import "ComparatorsContainerConvenience.h"
 
 #if TARGET_OS_IPHONE
 #import "UIGeometryCompareEqual.h"
@@ -77,6 +76,35 @@
 #import "UIKitComparatorsContainer.h"
 #endif
 
-#import "NSMethodSignature+Cedar.h"
-#import "CDRBlockHelper.h"
+#ifdef CEDAR_CUSTOM_MATCHERS
+#import CEDAR_CUSTOM_MATCHERS
+#endif
+
+
+#pragma mark - Cedar Doubles
+
 #import "CDRNil.h"
+#import "CDRSpy.h"
+#import "CDRFake.h"
+#import "CDRSpyInfo.h"
+#import "CedarDouble.h"
+#import "CDRClassFake.h"
+#import "CDRBlockHelper.h"
+#import "CDRProtocolFake.h"
+#import "CDRTypeUtilities.h"
+#import "NSMethodSignature+Cedar.h"
+
+
+#pragma mark - Cedar Doubles/Matchers
+
+#import "Argument.h"
+#import "ReturnValue.h"
+#import "AnyArgument.h"
+#import "HaveReceived.h"
+#import "ValueArgument.h"
+#import "StubbedMethod.h"
+#import "RejectedMethod.h"
+#import "NSInvocation+Cedar.h"
+#import "AnyInstanceArgument.h"
+#import "AnyInstanceOfClassArgument.h"
+#import "AnyInstanceConformingToProtocolArgument.h"
