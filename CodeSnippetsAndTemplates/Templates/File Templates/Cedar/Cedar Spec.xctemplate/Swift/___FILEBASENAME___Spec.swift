4ca3f308c6df2fb4bf88888d0c0368a510c56292
Brian Croom <brian.s.croom@gmail.com>
2016-01-13 15:55:17 -0500

    Update the Cedar spec template to allow generating Swift specs

---
+++ CodeSnippetsAndTemplates/Templates/File Templates/Cedar/Cedar Spec.xctemplate/Swift/___FILEBASENAME___Spec.swift
@@ -0,0 +1,11 @@
+import Cedar
+
+class ___VARIABLE_cedarClass:identifier___Spec: CDRSpec {
+    override func declareBehaviors() {
+        var <#object under test#>: ___VARIABLE_cedarClass:identifier___!
+
+        beforeEach {
+
+        }
+    }
+}
